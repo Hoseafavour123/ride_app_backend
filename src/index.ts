be3fee032994d43708f39aa1da21524da3bb1cb7
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2025-05-27 23:50:04 +0100

    Port settings 5000

--- src/index.ts
+++ src/index.ts
@@ -47,7 +47,7 @@ app.use("/api/v1/sessions", authenticate, sessionRoutes);
 // error handler
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 7004;
+const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV || "development";
 
 app.listen(PORT, async () => {
